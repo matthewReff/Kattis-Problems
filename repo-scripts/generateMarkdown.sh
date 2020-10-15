b7337dd52da52662bee703234783480057cc3074
Matthew Reff <geniusammyralltheothernamesare@gmail.com>
2020-10-15 13:51:44 -0600

    Fixed the repo script not linking to the problem correctly

--- repo-scripts/generateMarkdown.sh
+++ repo-scripts/generateMarkdown.sh
@@ -1,12 +1,13 @@
 # add entensions as needed for the languages you use
 # *.cpp *.py *.c *.c# *.cc *.cs *.cxx *.go *.hs *.java *.js *.m *.pas *.php *.pl *.rb
 codePathPrefix="../code/"
-echo ../code/*.cpp ../code/*.py > temp.txt
+ReadmePath="../README.md"
+echo $codePathPrefix*.cpp $codePathPrefix*.py > temp.txt
 python3 seperateNames.py temp.txt > filenames.txt
-cat README.template > ../README.md
-echo "| Problem | Language | " >> ../README.md
-echo "|:-------:|:--------:| " >> ../README.md
-python3 createReadme.py filenames.txt >> ../README.md
+cat README.template > $ReadmePath
+echo "| Problem | Language | " >> $ReadmePath
+echo "|:-------:|:--------:| " >> $ReadmePath
+python3 createReadme.py filenames.txt >> $ReadmePath
 rm filenames.txt
 rm temp.txt
-git add ../README.md
+git add $ReadmePath
